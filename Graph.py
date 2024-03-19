c5270c6b15fa9df1d2759a5ab30e5263eed121a2
Catrin <catrin.marz@gmail.com>
2024-03-19 15:38:43 +0100

    Temperaturgraph in Python

---
+++ Graph.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import rrdtool
+filename="/var/www/html/temperatur1.rrd"
+
+ret = rrdtool.graph ('-' ,
+	'--imgformat', 'PNG' ,
+	'--width', '900' ,
+	'--height', '450' ,
+	'--start', '-2hour' ,
+	'--end', '-1hour' ,
+	'--vertical-label' , "grad Celsius" ,
+	'--alt-autoscale' ,
+	'--title', 'Temperatur' ,
+	'DEF:temp1='+filename+':temp1:AVERAGE' ,
+	'AREA:temp1#ADD8E6:' ,
+	'LINE1:temp1#0000FF:Temperatur DS18B20' ,
+	'GPRINT:temp1:MIN:Min\\: %3.2lf °C ' ,
+	'GPRINT:temp1:MAX:Max\\: %3.2lf °C ' ,
+	'GPRINT:temp1:AVERAGE:Avg\\: %3.2lf °C ' ,
+	'GPRINT:temp1:LAST:Aktuell\\: %3.2lf °C ')
+
+
